65cbf60771465e4cc4a41141612d2919bbc53427
Rim <bvt.vanthang2010@gmail.com>
2023-11-02 23:17:11 +0700

    update shop

---
+++ woocommerce/archive-product.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Shop page
+ * 
+ */
+?>
+<?php get_header(); ?>
+
+<main id="primary" class="site-main">
+    <?php get_template_part('template-parts/shop/hero'); ?>
+</main>
+
+
+<?php get_footer(); ?>
